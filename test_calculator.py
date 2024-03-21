1d510adcdf563cafdcabde24a8b34710f1c82149
Mihir Katyal <mkatyal@uci.edu>
2024-03-20 20:19:41 -0700

    added def test_addition

--- test_calculator.py
+++ test_calculator.py
@@ -4,3 +4,6 @@ from rpn_calculator import RPNCalculator
 class TestRPNCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = RPNCalculator()
+    
+    def test_addition(self):
+        self.assertEqual(self.calculator.evaluate_expression("3 5 +"), 8)
